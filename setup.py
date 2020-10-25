05765233d8053c4229283fa3f5a9a8a1946a60ee
jaryaman <j.aryaman25@gmail.com>
2020-10-25 16:29:06 +0000

    Add wheel as requirement

--- setup.py
+++ setup.py
@@ -4,11 +4,15 @@ import sys
 from setuptools import find_packages
 from setuptools import setup
 
+def readToList(fileName):
+    return [line.strip() for line in open(fileName).readlines()] 
 
 if '__main__' == __name__:
+    requirements = readToList('requirements.txt')
     setup(
         author='Juvid Aryaman',
         author_email='j.aryaman25@gmail.com',
+        install_requires = requirements,
         packages=find_packages(),
         license=open('LICENSE.txt').read(),
         long_description=open('README.md').read(),
