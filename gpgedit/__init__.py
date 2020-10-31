9adc33d8fe40f838b57fdbec8ac8f8ab668b1a60
Juvid Aryaman <j.aryaman25@gmail.com>
2020-10-31 13:34:58 +0000

    Mocking (#2)
    * Tactical commit
    
    * Add tests

--- gpgedit/__init__.py
+++ gpgedit/__init__.py
@@ -30,25 +30,27 @@ def _make_backup(file: 'File', backup_suffix=BACKUP_SUFFIX):
     return backup_file
 
 
-def _initialize_temp(path: PosixPath):
+def _initialize_temp(path: PosixPath, exist_ok = False):
     temp_file = File(path=path)
-    temp_file.mkdir()
+    temp_file.mkdir(exist_ok = exist_ok)
     os.chmod(temp_file.path.parent, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
     return temp_file
 
 
 def _decrypt_data_to_temp(data_file: 'File', temp_file: 'File', passwd: str):
-    cmd = "gpg -d --passphrase-fd 0 --output %s %s" % (str(temp_file.path), str(data_file.path))
+    cmd = "gpg -d --batch --no-tty --passphrase-fd 0 --output %s %s" % (str(temp_file.path), str(data_file.path))
     proc = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE)
     proc.stdin.write(bytes(passwd, 'utf-8'))
     proc.stdin.close()
     if proc.wait() != 0:
         raise Exception("Error decrypting file.")
 
+def _launch_editor(file: 'File'):
+    os.system('%s %s' % (EDITOR, str(file.path)))
 
 def _augment(file: 'File'):
     file.get_stats()
-    os.system('%s %s' % (EDITOR, str(file.path)))
+    _launch_editor(file)
     file.get_stats()
 
     if (file.stats[-1].st_mtime == file.stats[-2].st_mtime
@@ -57,7 +59,7 @@ def _augment(file: 'File'):
 
 
 def _encrypt_temp_to_data(temp_file: 'File', data_file: 'File', passwd: str):
-    cmd = "gpg -a --yes --symmetric --passphrase-fd 0 --output %s %s" % (str(data_file.path), str(temp_file.path))
+    cmd = "gpg -a --yes --batch --no-tty --symmetric --passphrase-fd 0 --output %s %s" % (str(data_file.path), str(temp_file.path))
     proc = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE)
     proc.stdin.write(bytes(passwd, 'utf-8'))
     proc.stdin.close()
@@ -100,13 +102,15 @@ def _prompt_for_path():
     else:
         return path
 
+def get_input_message():
+    return input('Message:')
 
 def generate_encrypted(data_file_path: PosixPath):
     if not data_file_path.parent.exists():
         data_file_path.parent.mkdir(exist_ok=True)
     
     data_file = File(data_file_path)
-    msg = input('Message:')
+    msg = get_input_message()
     msg += '\n'
     passwd = getpass.getpass()
 
--- test/test_gpgedit.py
+++ test/test_gpgedit.py
@@ -1,13 +1,22 @@
-import shutil
+
+import getpass
 import os
+import shutil
 from os import stat_result
 from pathlib import Path, PosixPath
 from unittest.mock import patch
 
-from gpgedit import File, edit_encrypted, generate_encrypted, get_input_file, _make_backup, BACKUP_SUFFIX
+import pytest
+from gpgedit import (BACKUP_SUFFIX, TMP_DIR, TMP_FILE_NAME, File, _augment,
+                     _decrypt_data_to_temp, _encrypt_temp_to_data,
+                     _file_changed, _initialize_temp, _launch_editor,
+                     _make_backup, edit_encrypted, generate_encrypted,
+                     get_input_file, get_input_message)
 
 # *** global variables ***
 TEST_FILE = 'test.gpg'
+RESOURCES = './resources'
+MESSAGE = '***Hello world!***'
 
 
 # *** functions ***
@@ -73,33 +82,152 @@ def test__make_backup():
     
 
 def test__initialize_temp():
-    # TODO
-    pass
+    temp_file = Path(TMP_DIR)/TMP_FILE_NAME
+    try:
+        out_file = _initialize_temp(temp_file, exist_ok=True)
+        assert temp_file.parent.exists()
+        assert os.access(temp_file.parent, os.R_OK)
+        assert os.access(temp_file.parent, os.W_OK)
+        assert os.access(temp_file.parent, os.X_OK | os.W_OK)
+        assert isinstance(out_file, File)
+    except Exception as e:
+        raise e
+    finally:
+        shutil.rmtree(temp_file.parent)
 
 def test__decrypt_data_to_temp():
-    # TODO
+    original_file = File(Path(RESOURCES)/'test.txt')  
+    original_file.path.write_text('test text')
+
+    data_file = File(Path(TMP_DIR)/'test.gpg')
+    data_file.mkdir(exist_ok=True)
+    passwd = 'test-pswd'
+
+    enc_dec_file = File(Path(RESOURCES)/'test-dec.txt')
+    try:
+        _encrypt_temp_to_data(original_file, data_file, passwd)
+        _decrypt_data_to_temp(data_file, enc_dec_file, passwd)
+        assert original_file.path.read_text() == enc_dec_file.path.read_text()
+    except Exception as e:
+        raise e
+    finally:
+        shutil.rmtree(data_file.path.parent)
+        os.remove(original_file.path)
+        if enc_dec_file.path.exists():
+            os.remove(enc_dec_file.path)
+
+def test__launch_editor():
+    # Not sure how to test this!
     pass
 
-def test__augment():
-    # TODO
-    pass
+def test__augment_with_no_change(mocker):
+    mocker.patch('gpgedit._launch_editor')
+
+    test_file = File(Path(RESOURCES)/'test.txt')
+    test_file.path.write_text('test text\n')
+    with pytest.raises(Exception) as _:
+        _augment(test_file)    
+
+def test__augment_with_change(mocker):
+    test_file = File(Path(RESOURCES)/'test.txt')
+    original_message = 'test text\n'
+    test_file.path.write_text(original_message)
+    mocker.patch('gpgedit._launch_editor', new=overwrite_file)
+
+    try:
+        _augment(test_file)
+        assert test_file.path.read_text() != original_message
+    except Exception as e:
+        raise e
+    finally:
+        if test_file.path.exists():
+            os.remove(test_file.path)
 
 def test__encrypt_temp_to_data():
-    #TODO
-    pass
+    temp_file = File(Path(TMP_DIR)/'data-test.txt')
+    temp_file.mkdir(exist_ok=True)
+    temp_file.path.write_text('test text')
+
+    data_file = File(Path(RESOURCES)/'test.gpg')
+    passwd = 'test-pswd'
+    try:
+        _encrypt_temp_to_data(temp_file, data_file, passwd)
+        assert temp_file.path.exists()
+    except Exception as e:
+        raise e
+    finally:
+        if data_file.path.exists():
+            os.remove(data_file.path)
+        shutil.rmtree(temp_file.path.parent)
 
 def test__file_changed():
-    #TODO
-    pass
+    test_file = File(Path(RESOURCES)/'test.txt')
+    original_message = 'test text\n'
+    test_file.path.write_text(original_message)
+    test_file.get_stats()
+    
+    try:
+        assert not _file_changed(test_file)
+        test_file.path.write_text('new message')
+        assert _file_changed(test_file)
+        assert not _file_changed(test_file)
+    except Exception as e:
+        raise e
+    finally:
+        os.remove(test_file.path)
 
-def test_edit_encrypted():
-    #TODO
-    pass
+
+def test_edit_encrypted(mocker):
+    encrypted_file_path = Path(RESOURCES)/'test.gpg'
+    decrypted_file_path = Path(RESOURCES)/'decrypted.txt'
+    pswd = 'test-pass'
+    msg_original = 'this is a test'
+
+    generate_encrypted_dummy(mocker, encrypted_file_path, pswd=pswd, msg=msg_original)
+    
+    mocker.patch('getpass.getpass', return_value=pswd)
+    mocker.patch('gpgedit._augment', new=overwrite_file)
+    
+    try:
+        edit_encrypted(encrypted_file_path)
+        _decrypt_data_to_temp(File(encrypted_file_path), File(decrypted_file_path), pswd)
+        assert decrypted_file_path.read_text() == MESSAGE
+    except Exception as e:
+        raise e
+    finally:
+        os.remove(encrypted_file_path)
+        os.remove(decrypted_file_path)
+        
 
 def test__prompt_for_path():
-    # TODO
     pass
 
-def test_generate_encrypted():
-    # TODO
+def test_get_input_message():
     pass
+
+def test_generate_encrypted(mocker):
+    mocker.patch('gpgedit.get_input_message', return_value='test message')
+    mocker.patch('getpass.getpass', return_value='test-pswd')
+
+    temp_dir = Path(TMP_DIR)
+    try:
+        test_file = Path(RESOURCES)/'test.gpg'
+        generate_encrypted(test_file)
+        assert test_file.exists()
+        assert not temp_dir.exists()
+    except Exception as e:
+        raise e
+    finally:
+        os.remove(test_file)
+
+# *** functions ***
+
+def overwrite_file(file: 'File'):
+    file.path.write_text(MESSAGE)
+
+def generate_encrypted_dummy(mocker, path: Path, msg: str = 'test message', pswd: str = 'test-pswd'):
+    mocker.patch('gpgedit.get_input_message', return_value=msg)
+    mocker.patch('getpass.getpass', return_value=pswd)
+    generate_encrypted(path)
+
+# *** classes ***
